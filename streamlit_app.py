80ff5c81f08934d760b7ee50002cfb0d50606e3c
Anastasiia Zakharova <45105595+azakharova@users.noreply.github.com>
2023-11-29 14:31:28 +0100

    Update streamlit_app.py
    fixed typo
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('New healthy diner')
+streamlit.title('New healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-range Egg')
