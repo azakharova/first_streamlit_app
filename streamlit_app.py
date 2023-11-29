7ab30989c0a61c2fc677df6f2f05c494117a0206
Anastasiia Zakharova <45105595+azakharova@users.noreply.github.com>
2023-11-29 14:35:43 +0100

    Update streamlit_app.py
    changed breakfast header
    added emojis
    added 1 menu position

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('New healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
