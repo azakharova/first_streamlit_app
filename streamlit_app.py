0698d2ada04f8155acf91b3393e5642c93ff0bb1
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-11-30 11:32:34 +0100

    Update streamlit_app.py added searchbox for frut and a variable to store entered fruit name modified url call to use variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruitvice API response
 streamlit.header('Fruitvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# streamlit.text(fruityvice_response.json()) # Display the API response on the page
+fruit_choice = streamlit.text_input("What fruit do you want to know about?", "kiwi")
+streamlit.write("You selected", fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the API response
 fruitvice_normalize = pandas.json_normalize(fruityvice_response.json())
