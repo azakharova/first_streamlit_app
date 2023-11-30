e034db2f1a0fffeae9d6839112313e5b90414222
Anastasiia Zakharova <anastasia.zakharova.01.95@gmail.com>
2023-11-30 16:10:41 +0100

    Update streamlit_app.py added function to return snowflake table data added button to display snowflake table data

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+
 # import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,6 +30,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
+
 ## New section to display fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -47,15 +50,25 @@ except URLError as e:
         """
         % e.reason
     )
-streamlit.stop()
+
+
 
 # import snpwflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:  ")
-streamlit.dataframe(my_data_rows)
+
+## function to return the fruit load list from snowflake
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+## Add a button to load the fruit
+if streamlit.button("Get Fruit Load List"):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
+streamlit.stop()
 
 fruit_add = streamlit.text_input("What fruit do you want to add to the list?", "jackfruit")
 my_cur.execute("insert into fruit_load_list values ('" + fruit_add + "')")
